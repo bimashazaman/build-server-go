e0da10e9da69b18b424dea1c20a4c6169d36da4b
bimashazaman <bimashazaman1234@gmail.com>
2022-03-27 19:41:46 +0600

    go

--- hello.go
+++ hello.go
@@ -13,3 +13,137 @@ func main() {
 
 		fmt.printf("Starting server at port 8000")
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+func main() {
+    fileServer := http.FileServer(http.Dir("/.static"))
+		http.Handle("/", fileServer)
+		http.HandleFunc("/form", formHandler)
+		http.HandleFunc("/hello", helloHandler)
+
+		fmt.printf("Starting server at port 8000")
+}
+
+
+func main() {
+    fileServer := http.FileServer(http.Dir("/.static"))
+		http.Handle("/", fileServer)
+		http.HandleFunc("/form", formHandler)
+		http.HandleFunc("/hello", helloHandler)
+
+		fmt.printf("Starting server at port 8000")
+}
+
+
+func main() {
+    fileServer := http.FileServer(http.Dir("/.static"))
+		http.Handle("/", fileServer)
+		http.HandleFunc("/form", formHandler)
+		http.HandleFunc("/hello", helloHandler)
+
+		fmt.printf("Starting server at port 8000")
+}
+
+
+func main() {
+    fileServer := http.FileServer(http.Dir("/.static"))
+		http.Handle("/", fileServer)
+		http.HandleFunc("/form", formHandler)
+		http.HandleFunc("/hello", helloHandler)
+
+		fmt.printf("Starting server at port 8000")
+}
+
+
+func main() {
+    fileServer := http.FileServer(http.Dir("/.static"))
+		http.Handle("/", fileServer)
+		http.HandleFunc("/form", formHandler)
+		http.HandleFunc("/hello", helloHandler)
+
+		fmt.printf("Starting server at port 8000")
+}
+
+
+func main() {
+    fileServer := http.FileServer(http.Dir("/.static"))
+		http.Handle("/", fileServer)
+		http.HandleFunc("/form", formHandler)
+		http.HandleFunc("/hello", helloHandler)
+
+		fmt.printf("Starting server at port 8000")
+}
+
+
+func main() {
+    fileServer := http.FileServer(http.Dir("/.static"))
+		http.Handle("/", fileServer)
+		http.HandleFunc("/form", formHandler)
+		http.HandleFunc("/hello", helloHandler)
+
+		fmt.printf("Starting server at port 8000")
+}
