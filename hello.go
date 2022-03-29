7ed40448cba8ed384011dc2de97a12f7dc510701
bimashazaman <bimashazaman1234@gmail.com>
2022-03-30 03:05:13 +0600

    go

--- hello.go
+++ hello.go
@@ -1,149 +1,44 @@
+
 package main
 
-import ("fmt"
-		"net/http"
-		"log"
+import (
+	"fmt"
+	"log"
+	"net/http"
 )
 
-func main() {
-    fileServer := http.FileServer(http.Dir("/.static"))
-		http.Handle("/", fileServer)
-		http.HandleFunc("/form", formHandler)
-		http.HandleFunc("/hello", helloHandler)
-
-		fmt.printf("Starting server at port 8000")
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+	fmt.Fprintf(w, "POST request successful")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+	fmt.Fprintf(w, "Name = %s\n", name)
+	fmt.Fprintf(w, "Address = %s\n", address)
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-func main() {
-    fileServer := http.FileServer(http.Dir("/.static"))
-		http.Handle("/", fileServer)
-		http.HandleFunc("/form", formHandler)
-		http.HandleFunc("/hello", helloHandler)
-
-		fmt.printf("Starting server at port 8000")
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "method is not supported", http.StatusNotFound)
+		return
+	}
+	fmt.Fprintf(w, "hello!")
 }
 
-
 func main() {
-    fileServer := http.FileServer(http.Dir("/.static"))
-		http.Handle("/", fileServer)
-		http.HandleFunc("/form", formHandler)
-		http.HandleFunc("/hello", helloHandler)
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
 
-		fmt.printf("Starting server at port 8000")
-}
-
-
-func main() {
-    fileServer := http.FileServer(http.Dir("/.static"))
-		http.Handle("/", fileServer)
-		http.HandleFunc("/form", formHandler)
-		http.HandleFunc("/hello", helloHandler)
-
-		fmt.printf("Starting server at port 8000")
-}
-
-
-func main() {
-    fileServer := http.FileServer(http.Dir("/.static"))
-		http.Handle("/", fileServer)
-		http.HandleFunc("/form", formHandler)
-		http.HandleFunc("/hello", helloHandler)
-
-		fmt.printf("Starting server at port 8000")
-}
-
-
-func main() {
-    fileServer := http.FileServer(http.Dir("/.static"))
-		http.Handle("/", fileServer)
-		http.HandleFunc("/form", formHandler)
-		http.HandleFunc("/hello", helloHandler)
-
-		fmt.printf("Starting server at port 8000")
-}
-
-
-func main() {
-    fileServer := http.FileServer(http.Dir("/.static"))
-		http.Handle("/", fileServer)
-		http.HandleFunc("/form", formHandler)
-		http.HandleFunc("/hello", helloHandler)
-
-		fmt.printf("Starting server at port 8000")
-}
-
-
-func main() {
-    fileServer := http.FileServer(http.Dir("/.static"))
-		http.Handle("/", fileServer)
-		http.HandleFunc("/form", formHandler)
-		http.HandleFunc("/hello", helloHandler)
-
-		fmt.printf("Starting server at port 8000")
+	fmt.Printf("Starting server at port 8080\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
 }
